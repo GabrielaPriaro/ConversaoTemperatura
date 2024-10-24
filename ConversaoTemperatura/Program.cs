4a82a9425a1ca25c38e31c408ad4e2dc162f1744
Gabriela <gabriela.priaro@gmail.com>
2024-10-24 13:15:40 +0100

    Adicionar arquivos de projeto.

---
+++ ConversaoTemperatura/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConversaoTemperatura
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
